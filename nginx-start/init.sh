ce1542c210b36cc1d66808520aeae1ba3ff7302a
Jaysonzhao <jaysonzhao@vip.qq.com>
2023-12-11 21:16:53 +0800

    Update init.sh

--- nginx-start/init.sh
+++ nginx-start/init.sh
@@ -1,2 +1,2 @@
-envsubst '${NODEIP},${SVCNAME},${SVCPORT}' < /opt/app-root/src/nginx-proxy.conf > output.conf
-cp output.conf /opt/app-root/etc/nginx.default.d/nginx-proxy.conf
+# envsubst '${NODEIP},${SVCNAME},${SVCPORT}' < /opt/app-root/src/nginx-proxy.conf > output.conf
+# cp output.conf /opt/app-root/etc/nginx.default.d/nginx-proxy.conf
